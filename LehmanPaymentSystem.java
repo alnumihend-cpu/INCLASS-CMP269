cded63d3369aaf45fb5f5e12952fdf82809baae1
alnumihend-cpu <alnumihend@gmail.com>
2026-02-06 20:32:53 -0500

    Update LehmanPaymentSystem.java

--- LehmanPaymentSystem.java
+++ LehmanPaymentSystem.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 
 public class LehmanPaymentSystem {
 	public static void main(String[] args) {
-		ArrayList<Payable> pending = new ArrayList<>();
+		ArrayList<Payable> paymentQueue = new ArrayList<>();
 		
 		CreditCard card = new CreditCard("there's", 60.0, 100.0);
 		MealPlan meal = new MealPlan("Yours", 40.0);
 		
-		pending.add(card);
-		pending.add(meal);
+		paymentQueue.add(card);
+		paymentQueue.add(meal);
 		
-		for(Payable p: pending) {
+		for(Payable p: paymentQueue) {
 			p.processPayment(50.0);
 			System.out.println(" Card Status: "  + p.getPaymentSituation());
 			System.out.println("*************************");

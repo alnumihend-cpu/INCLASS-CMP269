1ae96441dd9ffcb2f9831acbe19ff16b40304e49
alnumihend-cpu <alnumihend@gmail.com>
2026-03-02 01:30:38 -0500

    Update Exercise2.java

--- Exercise2.java
+++ Exercise2.java
@@ -4,13 +4,13 @@ public class Exercise2 {
 	public static void main(String[] args) throws InterruptedException{
 		Thread t = new Thread(() -> {
             try {
-                Thread.sleep(2000);  // Sleep for 2 seconds
+                Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
 
-	
+		//Statments and starts
 		System.out.println("After creation: " + t.getState());
 		t.start();
 		

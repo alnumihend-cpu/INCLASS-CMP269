a4ce37912b383635ba02bc063f5ebd1a97afcc7f
alnumihend-cpu <alnumihend@gmail.com>
2026-04-30 14:29:36 -0400

    Add files via upload

---
+++ Sealed Classes & "When" Expressions.kt
@@ -0,0 +1,24 @@
+sealed class EnrollmentStatus
+
+data class Success(val courseCode:String) : EnrollmentStatus()
+data class Error(val message: String):EnrollmentStatus()
+object Loading : EnrollmentStatus()
+//object Isuccess : EnrollmentStatus()
+
+fun printStatus(status: EnrollmentStatus) {
+    when (status) {
+        //is Isuccess ->println("S
+        is Success -> println("Enrolled in: ${status.courseCode}")
+        is Error -> println("Error:${status.message}")
+        is Loading -> println("Loading...")
+    }
+}
+
+fun main() {
+
+    val successStatus = Success("CS101")
+    val errorStatus = Error("Course is full")
+
+    printStatus(successStatus)
+    printStatus(errorStatus)
+}
